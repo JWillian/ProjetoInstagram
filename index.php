f17549170de4e001f975dd941b6c4189d85fe8eb
Jonatas Willian <jonatasw40@gmail.com>
2021-08-25 15:40:33 -0300

    Insta new version

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php
-<<<<<<< HEAD
+
 require 'includes/header.php';
 require 'includes/cabecalho.php';
-require 'includes/section.php';
+require 'includes/body.php';
 require 'includes/rodape.php';
 require 'includes/footer.php';
-=======
 
-require 'header.php';
-require 'cabecalho.php';
-require 'body.php';
-require 'rodape.php';
-require 'footer.php';
-
->>>>>>> c062b3f03a08eed81e983c8855cccdc4af7cf050
 ?>
